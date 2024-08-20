8af2f37ac86e06ff50c2de21d1d58817887254f6
xiangshanxs <153355729+xiangshanxs@users.noreply.github.com>
2024-08-21 00:04:02 +0800

    Update Contingency.m

--- Contingency.m
+++ Contingency.m
@@ -9,13 +9,13 @@ function Cont=Contingency(Mem1,Mem2)
 
 %(C) David Corney (2000)   		D.Corney@cs.ucl.ac.uk
 
-if nargin < 2 | min(size(Mem1)) > 1 | min(size(Mem2)) > 1
+if nargin < 2 || min(size(Mem1)) > 1 || min(size(Mem2)) > 1
    error('Contingency: Requires two vector arguments')
-   return
+%   return
 end
 
 Cont=zeros(max(Mem1),max(Mem2));
 
-for i = 1:length(Mem1);
+for i = 1:length(Mem1)
    Cont(Mem1(i),Mem2(i))=Cont(Mem1(i),Mem2(i))+1;
 end
